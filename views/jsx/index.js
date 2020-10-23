98c9bd427849f7ac4fe75eea412db4d1e5c34d08
Gennady Terekhov <ter.ge@yandex.ru>
2020-10-23 18:37:28 +0300

    players table works

--- views/jsx/index.js
+++ views/jsx/index.js
@@ -3,191 +3,31 @@
 const formElement = React.createElement;
 
 
-class GameWord extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.value,
-            // index: props.key
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <span>
-                    {/* {this.props.key} */}
-                </span> 
-                <span>
-                    {this.props.value}
-                </span>
-                <br></br>
-            </div>
-        );
-    }
-}
-
-
-
-
-
-
-
-
-
-class GameMessage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: props.text,
-        };
-    }
-  
-    render() {
-        return (
-            <div>
-                <p>{this.props.text}</p>
-            </div>
-        );
-    }
-}
-
-
-
-
-
-
-
-
-
-
-class PlayerComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: props.name,
-            score: props.score
-        };
-    }
-
-    findPlayerByName(name) {
-
-    }
-
-
-    render() {
-        return (
-            <div>{this.state.name} {this.state.score}</div>
-        );
-    }
-}
-
-
-class Player {
-    constructor(name) {
-        this.name = name;
-        this.score = 0;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class GamePlayersTable extends React.Component {
-    constructor(props) {
-        super(props);
-        // let number = props.playersNumber;
-        // let names = [];
-        // for (let i = 1; i <= number; i += 1) {
-        //     names.push(`Игрок ${i}`);
-        // }
-        this.state = {
-            playersData: props.playersData,
-        };
-    }
-  
-    getPlayerScore(playerName) {
-        return 0;
-    }
-
-    render() {
-        return (
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            {/* <td>#</td> */}
-                            <td>игрок</td>
-                            <td>очки</td>
-                        </tr>
-
-                    </thead>
-                    <tbody>
-
-                        {this.state.playersData.map(
-                            (value, index) => (
-                                <tr key={index}>
-                                    {/* <td>{index}</td> */}
-                                    <td>{value.name}</td>
-                                    <td>{value.score}</td>
-                                </tr>
-                            )
-                        )}
-
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
-}
-
-
-
-// className="players-table-td"
-// className="players-table-td"
-// className="players-table-td"
-// className="players-table-td"
-// className="players-table-td"
-// className="players-table-td"
-
-
-
-
-
-
-
-
-
-
-
-class EnterWordForm extends React.Component {
+class MainGameComponent extends React.Component {
     constructor(props) {
         super(props);
 
+        let players = [
+            {name: 'Игрок 1', score: 0},
+            {name: 'Игрок 2', score: 0}
+        ];
 
 
         this.state = {
-            placeholder: props.placeholder,
-            value: props.value,
-            message: props.message,
-            mistakes: props.mistakes,
-            wordsReverseHtml: props.wordsReverseHtml,
-            wordsList: props.wordsList,
+            placeholder: 'введите ваше слово сюда',
+            value: '',
+            message: 'Игра началась. Введите ваше первое слово',
+            mistakes: 0,
+            wordsReverseHtml: [],
+            wordsList: [],
+            playersData: players,
         };
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+
 
     isWord(word) {
         // потом нужно будет сделать через викисловарь если у него есть АПИ
@@ -218,10 +58,14 @@ class EnterWordForm extends React.Component {
     }
 
 
+
     addWordToList(word) {
-        let temp = this.state.wordsList;
+        console.log('addWordToList');
+        let temp = this.state.wordsList.slice();
         temp.push(word);
         this.setState({wordsList: temp});
+        console.log('this.state');
+        console.log(this.state.wordsList);
     }
 
 
@@ -229,53 +73,86 @@ class EnterWordForm extends React.Component {
         this.setState({value: event.target.value});
     }
 
-
     handleSubmit(event) {
+        console.log('handleSubmit');
+        console.log(this.state.wordsList);
 
         let submittedWord = this.state.value;
         let checkResult = this.checkWord(submittedWord);
+        let message = '';
+        let updatedWordList = this.state.wordsList.slice();
+
+        let updatedPlayersData = this.state.playersData.slice();
+        const currentPlayerIndex = this.state.wordsList.length % 2;
+        
 
         if (checkResult.ok) {
-            this.addWordToList(submittedWord);
+            console.log('[handleSubmit] about to add a word to list');
+            // this.addWordToList(submittedWord);
+            updatedWordList.push(submittedWord);
+            console.log('[handleSubmit] after addition to list');
+            console.log(this.state.wordsList);
 
             let lastWordLastLetter = submittedWord[submittedWord.length - 1];
 
-            this.setState({
-                value: '',
-                message: `Слово "${submittedWord}" принято. Следующее слово должно начаться с буквы "${lastWordLastLetter}"`,
-            });
+            updatedPlayersData[currentPlayerIndex].score += 1;
+            // 
+            // 
+            // 
+            
+            // this.setState({
+            //     value: '',
+            //     message: `Слово "${submittedWord}" принято. Следующее слово должно начаться с буквы "${lastWordLastLetter}"`,
+            //     // playersData: updatedPlayersData,
+            // });
+            message = `Слово "${submittedWord}" принято. Следующее слово должно начаться с буквы "${lastWordLastLetter}"`;
+            console.log(this.state);
 
         } else {
-            this.setState({
-                mistakes: this.state.mistakes + 1,
-                message: `Слово "${submittedWord}" не принято. Причина: ` + checkResult.reason
-            })
+            // this.setState({
+            //     mistakes: this.state.mistakes + 1,
+            //     message: `Слово "${submittedWord}" не принято. Причина: ` + checkResult.reason
+            // })
+            message = `Слово "${submittedWord}" не принято. Причина: ` + checkResult.reason;
         }
-        this.setState({wordsReverseHtml: this.state.wordsList.slice().reverse()});
-        
-        // alert('A word was submitted: ' + this.state.value);
+
+        console.log(this.state.wordsList);
+
+
+
+
+
+        this.setState({
+            value: '',
+            message,
+            wordsList: updatedWordList,
+            playersData: updatedPlayersData
+            // wordsReverseHtml: this.state.wordsList.slice().reverse()
+        });
+
+        console.log(this.state.wordsList);
+
         event.preventDefault();
     }
 
+
+
     render() {
         return (
-            <div id="mainReturnedElement">
-                
-                 <div id="wordEnterForm">
+            <div className="row">
+                <div className="col">
+
+
+                    всего слов: {this.state.wordsList.length}
+                    <br></br>
+
+                    {this.state.message}
+
+                    <br></br>
+
                     <form onSubmit={this.handleSubmit}>
-
-                        всего слов: {this.state.wordsList.length}
-                        <br></br>
-                        {/* всего ошибок: {this.state.mistakes}
-                        <br></br> */}
-
-                        <GameMessage text={this.state.message}></GameMessage>
-                        <br></br>
-
                         <input
                             type="text"
-                            // player_index={this.state.currentPlayerIndex}
-                            // player={this.state.currentPlayerName}
                             placeholder={this.state.placeholder}
                             value={this.state.value} 
                             onChange={this.handleChange}
@@ -284,70 +161,43 @@ class EnterWordForm extends React.Component {
                         <input type="submit" value="Отправить" />
                     </form>
 
+                    <br></br>
+                    <ol reversed>
+                        {this.state.wordsList.slice().reverse().map(
+                            (value, index) => (
+                                <li key={index}> {value}</li>
+                            )
+                        )}
+                    </ol>
                 </div>
-                <div id="wordsTable">
-                    {this.state.wordsReverseHtml.map(
-                        (value, index) => (
-                            <GameWord value={value} key={index} />
-                        )
-                    )}
-                </div> 
-            </div>
-        );
-    }
-}
 
 
-
-
-
-
-class MainGameComponent extends React.Component {
-    constructor(props) {
-        super(props);
-
-        // let players = [
-        //     new Player({
-        //         name: 'Игрок 1',
-        //         score: 0
-        //     }),
-        //     new Player({
-        //         name: 'Игрок 2',
-        //         score: 0
-        //     })
-        // ];
-        let players = [
-            new Player('Игрок 1'),
-            new Player('Игрок 2')
-        ];
-
-        this.state = {
-            placeholder: 'введите ваше слово сюда',
-            value: '',
-            message: 'Игра началась. Введите ваше первое слово',
-            mistakes: 0,
-            wordsReverseHtml: [],
-            wordsList: [],
-            playersData: players,
-        };
-    }
-
-    render() {
-        return (
-            <div className="row">
                 <div className="col">
-                    <EnterWordForm
-                        placeholder={this.state.placeholder}
-                        value={this.state.value}
-                        message={this.state.message}
-                        mistakes={this.state.mistakes}
-                        wordsReverseHtml={this.state.wordsReverseHtml}
-                        wordsList={this.state.wordsList}
-                        handleSubmit={this.handleSubmit}
-                    />
-                </div>
-                <div className="col">
-                    <GamePlayersTable playersData={this.state.playersData} />
+
+                    <table>
+                        <thead>
+                            <tr>
+                                {/* <td>#</td> */}
+                                <td>игрок</td>
+                                <td>очки</td>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+
+                            {this.state.playersData.map(
+                                (value, index) => (
+                                    <tr key={index}>
+                                        {/* <td>{index}</td> */}
+                                        <td>{value.name}</td>
+                                        <td>{value.score}</td>
+                                    </tr>
+                                )
+                            )}
+
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         );
